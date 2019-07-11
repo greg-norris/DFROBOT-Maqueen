1f2f7d7de499100e1a2bad8ef35f6b15f131256f
JasonLS <jsedluk01@gmail.com>
2019-07-11 15:20:24 -0400

    Updates.

--- Maqueen.ts
+++ Maqueen.ts
@@ -1,5 +1,5 @@
 let alreadyInit = 0
-let strip = light.createNeoPixelStrip(pins.P15, 4)
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 const MOTER_ADDRESSS = 0x10
 
 enum PingUnit {
